be23ddedac956e8efb1a38ab098864a7fd0a614a
Coslty <kengyin123@gmail.com>
2017-10-21 13:37:51 -0800

    change

---
+++ app/src/main/java/com/example/anthonynguyen/sdhack17_01/RecyclerViewAdapter.java
@@ -0,0 +1,13 @@
+package com.example.anthonynguyen.sdhack17_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Adapter;
+
+/**
+ * Created by Keng on 10/21/2017.
+ */
+
+public class RecyclerViewAdapter extends AppCompatActivity {
+
+
+}
