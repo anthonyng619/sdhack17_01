bfcc3d0890aa0154030ba598e539332a437e887f
Coslty <kengyin123@gmail.com>
2017-10-21 20:58:59 -0800

    change

--- app/src/main/java/com/example/anthonynguyen/sdhack17_01/ExtraActivity.java
+++ app/src/main/java/com/example/anthonynguyen/sdhack17_01/ExtraActivity.java
@@ -1,16 +1,27 @@
 package com.example.anthonynguyen.sdhack17_01;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class ExtraActivity extends AppCompatActivity {
-
+    Button btn_camera;
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_extra);
+
+        btn_camera = (Button) findViewById(R.id.btn_camera);
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ExtraActivity.this, CameraActivity.class));
+            }
+        });
 
 
     }
